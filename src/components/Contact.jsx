479d1bf5dec767abba435ccb6df45ba052910e56
Sandesh Bankar <sandeshbankar24@gmail.com>
2024-12-16 11:56:16 +0530

    Transiton added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { motion } from "motion/react"
 
 const Contact = () => {
   return (
-    <div className='text-center p-6 py-20 lg:px-32  w-full overflow-hidden' i='Contact'>
+    <motion.div initial={{opacity:0, x:-200}}
+    transition={{duration:1}}
+    whileInView={{opacity:1, x:0}}
+    viewport={{once:true}} className='text-center p-6 py-20 lg:px-32  w-full overflow-hidden' i='Contact'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact 
         <span className='underline underline-offset-4 decoration-1 under font-light'> With Us</span></h1> 
         <p className='text-center text-gray-500  mb-12 max-w-80 mx-auto'>Ready to make a move</p>
@@ -31,7 +35,7 @@ const Contact = () => {
 
 
 
-    </div>
+    </motion.div>
   )
 }
 
